23c94640009c0848af7b2cad006f007c2c6cdae7
Chloe Marshall <chloe.r.marshall@gmail.com>
2017-02-21 23:05:11 -0600

    First Commit

--- WidgetProject/ViewController.h
+++ WidgetProject/ViewController.h
@@ -9,7 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *buttonLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *switchLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *switcher;
+@property (weak, nonatomic) IBOutlet UILabel *segmentLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segment;
+@property (weak, nonatomic) IBOutlet UIButton *updateTextfieldButton;
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+@property (weak, nonatomic) IBOutlet UILabel *textfieldLabel;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
 
+- (IBAction)buttonPressed:(id)sender;
+- (IBAction)segementSelector:(id)sender;
+- (IBAction)sliderValueChanged:(id)sender;
+- (IBAction)switchPressed:(id)sender;
+- (IBAction)updateButtonPressed:(id)sender;
 
 @end
 
--- WidgetProject/ViewController.m
+++ WidgetProject/ViewController.m
@@ -13,10 +13,12 @@
 @end
 
 @implementation ViewController
+int counter;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    counter = 0;
 }
 
 
@@ -26,4 +28,37 @@
 }
 
 
+- (IBAction)buttonPressed:(id)sender {
+    ++counter;
+    _buttonLabel.text = [[NSString alloc] initWithFormat:@"Button was pressed %d times", counter ];
+}
+
+- (IBAction)segementSelector:(id)sender {
+    if(_segment.selectedSegmentIndex == 0){
+        _segmentLabel.text = @"You selected segment #1";
+    }else if(_segment.selectedSegmentIndex == 1){
+        _segmentLabel.text = @"You selected segment #2";
+    }else{
+        _segmentLabel.text = @"No segment selected";
+
+    }
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    _sliderLabel.text = [[NSString alloc] initWithFormat:@"%f", _slider.value];
+}
+
+- (IBAction)switchPressed:(id)sender {
+    if(_switcher.on){
+        _switchLabel.text = @"The switch is on";
+    }else{
+        _switchLabel.text = @"The switch is off";
+        
+    }
+}
+
+- (IBAction)updateButtonPressed:(id)sender {
+    
+    _textfieldLabel.text = _textfield.text;
+}
 @end
